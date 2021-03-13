c183527265179968e6c20e18cc31d18c3f8efb8f
paulo sandim <paulo@cacdigital.com.br>
2021-03-13 16:06:53 -0300

    validando logins

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source "https://rubygems.org"
 
-gem "capybara", "3.33.0"
-gem "rspec", "3.9.0"
+gem "capybara", "3.35.3"
+gem "rspec", "3.10.0 "
 gem "selenium-webdriver", "3.142.7"
-gem "ffi", "1.13.1"
+gem "ffi", "1.15.0"
--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -1,6 +1,7 @@
 describe 'Forms' do
 
     it 'login com sucesso' do
+
         visit 'http://training-wheels-protocol.herokuapp.com/login'
         
         fill_in 'userId', with: 'stark'
@@ -9,6 +10,38 @@ describe 'Forms' do
         click_button 'Login'
         
         expect(find('#flash').visible?).to be true
+
+        # obtendo o texto e perguntando se o valor contém no texto
+        expect(find('#flash').text).to include 'Olá, Tony Stark. Você acessou a área logada!'
+
+        # perguntar se no elemento contém no texto
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    end
+
+    it 'senha incorreta' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+        
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'jarvis'
+        
+        click_button 'Login'
+
+        expect(find('#flash').visible?).to be true
+        
+        expect(find('#flash')).to have_content 'Senha é invalida!'
+    end
+
+    it 'usuário não cadastrado' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+        
+        fill_in 'userId', with: 'steve'
+        fill_in 'password', with: 'jarvis!'
+        
+        click_button 'Login'
+
+        expect(find('#flash').visible?).to be true
+        
+        expect(find('#flash')).to have_content 'O usuário informado não está cadastrado!'
     end
 
 end
